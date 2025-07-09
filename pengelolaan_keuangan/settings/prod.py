9c665ed4cee80c6881850b5f88fa9f4dad0dc742
dodiman <dodimantakimpoo@gmail.com>
2025-07-09 09:26:07 +0800

    modifikasi: file allowed_host

--- pengelolaan_keuangan/settings/prod.py
+++ pengelolaan_keuangan/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default="").split(",")
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['webtkp.pythonanywhere.com']
 DEBUG = False
