db6493240fa1bfed6ad14d5bc615731fd8b0b496
dodiman <dodimantakimpoo@gmail.com>
2025-07-11 20:17:38 +0800

    modif: header jadikan fix

--- static/js/komponen/toggleButton.js
+++ static/js/komponen/toggleButton.js
@@ -5,7 +5,20 @@ const elm_icon_close = document.getElementById('icon-close');
 
 elm_tombol_open_close_sidebar.addEventListener("click", (e) => {
     e.preventDefault();
-    elm_sidebar.classList.toggle('hidden');
-    elm_icon_burger.classList.toggle('hidden');
-    elm_icon_close.classList.toggle('hidden');
+    // tampil & sembunyikan sidebar
+    if (elm_sidebar.classList.contains("hidden")) {
+        elm_sidebar.classList.remove('hidden');
+        elm_sidebar.classList.add("flex")
+        elm_icon_burger.classList.remove('hidden');
+        elm_icon_burger.classList.add("flex")
+        elm_icon_close.classList.remove('hidden');
+        elm_icon_burger.classList.add("flex")
+    } else {
+        elm_sidebar.classList.remove("flex");
+        elm_sidebar.classList.add("hidden")
+        elm_icon_burger.classList.remove("flex");
+        elm_icon_burger.classList.add("hidden");
+        elm_icon_close.classList.remove('flex');
+        elm_icon_close.classList.add('hidden');
+    }
 })
